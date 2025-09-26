76fe1d3e6070e3f5642c3e2bdcda48f9b939dff9
ddezue <LYANAZAYKA@MAIL.RU>
2025-09-26 09:03:54 +0700

    Update 2.0 лаба 02.cpp

--- лаба 2.cpp
+++ лаба 2.cpp
@@ -2,11 +2,9 @@
  * Автор: Бакланова Ляна *
  * Вариант: 1            *
  *************************/
- 
+
 #include <iostream>
 #include <cmath>
-#include <iomanip>
-#include <vector>
 
 using namespace std;
 
@@ -15,18 +13,23 @@ int main() {
   double g = 9.81;
   double m = 75.0;
   double k = 8.0;
+  double t = 0.0;
   double v;
-  
-  vector <double> t_values = {0, 0.25, 0.5, 0.75, 1.0, 2.0, 3.0, 4.0, 5.0};
-  
+
   cout << "t (с)   v (м/с)" << endl;
-  cout << fixed << setprecision(3);
-  
-  for (double t : t_values) {
-    v = sqrt(g * m / k) * tanh (t * sqrt((g * k / m)));
+  cout << fixed;
+  cout.precision(3);
+
+  while (t<1) {
+    v = sqrt(g * m / k) * tanh(t * sqrt((g * k / m)));
     cout << t  << "\t" << v << endl;
+    t+=0.25;
   }
+
+  do {
+    v = sqrt(g * m / k) * tanh(t * sqrt((g * k / m)));
+    cout << t  << "\t" << v << endl;
+    t+=1; } while (t<6);
     
   return 0;
-
 }
